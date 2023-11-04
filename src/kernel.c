9a15f9799c9dc29892d02723f38755cf938dd1fb
andriy_yushyn <baybaraandrey@gmail.com>
2023-11-04 19:37:57 -0400

    print char to video memory

--- src/kernel.c
+++ src/kernel.c
@@ -1,7 +1,15 @@
 #include "kernel.h"
-
+#include <stdint.h>
 
 void kernel_main()
 {
+	char * video_mem = (char*)(0xB8000);
+	video_mem[0] = 'a';	
+	video_mem[1] = 3;	
 	
+	video_mem[2] = 'c';	
+	video_mem[3] = 3;	
+	
+	video_mem[4] = 'b';	
+	video_mem[5] = 3;	
 }
