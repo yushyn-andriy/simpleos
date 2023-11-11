5dc79b48de8389c50eb1970d30f07b2350c1b1cc
andriy_yushyn <baybaraandrey@gmail.com>
2023-11-11 14:03:47 -0500

    !!!FIX MISTAKE RELATED TO HEAP!!! wrong constant

--- src/memory/heap/kheap.c
+++ src/memory/heap/kheap.c
@@ -7,18 +7,19 @@
 struct heap kernel_heap;
 struct heap_table kernel_heap_table;
 
-
 void kheap_init()
 {
     int total_table_entries = SIMPLEOS_HEAP_SIZE_BYTES / SIMPLEOS_HEAP_BLOCK_SIZE;
-    kernel_heap_table.entries = (HEAP_BLOCK_TABLE_ENTRY*)(SIMPLEOS_HEAP_ADDRESS);
+    kernel_heap_table.entries = (HEAP_BLOCK_TABLE_ENTRY*)(SIMPLEOS_HEAP_TABLE_ADDRESS);
     kernel_heap_table.total = total_table_entries;
 
-    void *end = (void*)(SIMPLEOS_HEAP_ADDRESS + SIMPLEOS_HEAP_SIZE_BYTES);
-    int ret = heap_create(&kernel_heap, (void*)(SIMPLEOS_HEAP_ADDRESS), end, &kernel_heap_table);
-    if( ret < 0 ) {
+    void* end = (void*)(SIMPLEOS_HEAP_ADDRESS + SIMPLEOS_HEAP_SIZE_BYTES);
+    int res = heap_create(&kernel_heap, (void*)(SIMPLEOS_HEAP_ADDRESS), end, &kernel_heap_table);
+    if (res < 0)
+    {
         print("Failed to create heap\n");
     }
+
 }
 
 void *kmalloc(size_t size)
