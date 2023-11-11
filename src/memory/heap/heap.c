5a8119ce872a2e64563fe382ada7a98ffa61fb26
andriy_yushyn <baybaraandrey@gmail.com>
2023-11-11 14:04:10 -0500

    reformat a litle bit

--- src/memory/heap/heap.c
+++ src/memory/heap/heap.c
@@ -5,35 +5,32 @@
 #include <stdbool.h>
 
 
-static int heap_validate_table(void *ptr, void *end, struct heap_table *table)
+static int heap_validate_table(void* ptr, void* end, struct heap_table* table)
 {
     int res = 0;
 
     size_t table_size = (size_t)(end - ptr);
     size_t total_blocks = table_size / SIMPLEOS_HEAP_BLOCK_SIZE;
-
-    if(table->total != total_blocks)
+    if (table->total != total_blocks)
     {
         res = -EINVARG;
         goto out;
     }
 
-
 out:
     return res;
-} 
+}
 
-static bool heap_validate_allignment(void *ptr)
+static bool heap_validate_alignment(void* ptr)
 {
     return ((unsigned int)ptr % SIMPLEOS_HEAP_BLOCK_SIZE) == 0;
 }
 
-int heap_create(struct heap *heap, void *ptr, void *end, struct heap_table *table)
+int heap_create(struct heap* heap, void* ptr, void* end, struct heap_table* table)
 {
     int res = 0;
 
-
-    if(!heap_validate_allignment(ptr) || !heap_validate_allignment(end))
+    if (!heap_validate_alignment(ptr) || !heap_validate_alignment(end))
     {
         res = -EINVARG;
         goto out;
@@ -44,7 +41,7 @@ int heap_create(struct heap *heap, void *ptr, void *end, struct heap_table *tabl
     heap->table = table;
 
     res = heap_validate_table(ptr, end, table);
-    if( res < 0)
+    if (res < 0)
     {
         goto out;
     }
@@ -52,19 +49,21 @@ int heap_create(struct heap *heap, void *ptr, void *end, struct heap_table *tabl
 
     size_t table_size = sizeof(HEAP_BLOCK_TABLE_ENTRY) * table->total;
     memset(table->entries, HEAP_BLOCK_TABLE_ENTRY_FREE, table_size);
-    
+
 out:
     return res;
 }
 
 
+
 static uint32_t heap_align_value_to_upper(uint32_t val)
 {
-    if(( val % SIMPLEOS_HEAP_BLOCK_SIZE) == 0)
+    if ((val % SIMPLEOS_HEAP_BLOCK_SIZE) == 0)
     {
         return val;
     }
-    val = (val - (val % SIMPLEOS_HEAP_BLOCK_SIZE));
+
+    val = (val - ( val % SIMPLEOS_HEAP_BLOCK_SIZE));
     val += SIMPLEOS_HEAP_BLOCK_SIZE;
     return val;
 }
@@ -74,74 +73,86 @@ static int heap_get_entry_type(HEAP_BLOCK_TABLE_ENTRY entry)
     return entry & 0x0f;
 }
 
-int heap_get_start_block(struct heap *heap, uint32_t total_blocks)
+int heap_get_start_block(struct heap* heap, uint32_t total_blocks)
 {
-    struct heap_table *table = heap->table;
+    struct heap_table* table = heap->table;
     int bc = 0;
     int bs = -1;
 
-    for(size_t i = 0; i < table->total; i++)
+    for (size_t i = 0; i < table->total; i++)
     {
-        if(heap_get_entry_type(table->entries[i]) != HEAP_BLOCK_TABLE_ENTRY_FREE) {
+        if (heap_get_entry_type(table->entries[i]) != HEAP_BLOCK_TABLE_ENTRY_FREE)
+        {
             bc = 0;
             bs = -1;
             continue;
         }
 
+        // If this is the first block
         if (bs == -1)
         {
             bs = i;
         }
         bc++;
-
-        if (bc == total_blocks) break;
+        if (bc == total_blocks)
+        {
+            break;
+        }
     }
 
-    if(bs == -1) {
+    if (bs == -1)
+    {
         return -ENOMEM;
     }
+    
     return bs;
+
 }
 
 
-void *heap_block_to_address(struct heap *heap, int block)
+void* heap_block_to_address(struct heap* heap, int block)
 {
     return heap->saddr + (block * SIMPLEOS_HEAP_BLOCK_SIZE);
 }
 
-void heap_mark_blocks_taken(struct heap *heap, int start_block, int total_blocks)
+void heap_mark_blocks_taken(struct heap* heap, int start_block, int total_blocks)
 {
-    int end_block = (start_block + total_blocks) - 1;
+    int end_block = (start_block + total_blocks)-1;
+    
     HEAP_BLOCK_TABLE_ENTRY entry = HEAP_BLOCK_TABLE_ENTRY_TAKEN | HEAP_BLOCK_IS_FIRST;
-    if(total_blocks > 1)
+    if (total_blocks > 1)
     {
         entry |= HEAP_BLOCK_HAS_NEXT;
     }
-    for(int i = start_block; i <= end_block; i++)
+
+    for (int i = start_block; i <= end_block; i++)
     {
         heap->table->entries[i] = entry;
         entry = HEAP_BLOCK_TABLE_ENTRY_TAKEN;
-        if (i != end_block - 1)
+        if (i != end_block -1)
         {
             entry |= HEAP_BLOCK_HAS_NEXT;
         }
     }
 }
 
-void *heap_malloc_blocks(struct heap *heap, uint32_t total_blocks)
+void* heap_malloc_blocks(struct heap* heap, uint32_t total_blocks)
 {
-    void *address = 0;
+    void* address = 0;
+
     int start_block = heap_get_start_block(heap, total_blocks);
-    if (start_block < 0) goto out;
+    if (start_block < 0)
+    {
+        goto out;
+    }
 
     address = heap_block_to_address(heap, start_block);
 
-    // master block as taken
+    // Mark the blocks as taken
     heap_mark_blocks_taken(heap, start_block, total_blocks);
 
-
 out:
-    return address; 
+    return address;
 }
 
 void *heap_malloc(struct heap *heap, size_t size)
--- src/memory/paging/paging.asm
+++ src/memory/paging/paging.asm
@@ -5,12 +5,13 @@ section .asm
 global paging_load_directory
 global enable_paging
 
-
 paging_load_directory:
     push ebp
     mov ebp, esp
+
     mov eax, [ebp+8]
     mov cr3, eax
+    
     pop ebp
     ret
 
